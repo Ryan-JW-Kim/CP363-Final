b965cf61093a9abd83ba087a14668541ba991dd6
Ryan Kim <43964171+Ryan-JW-Kim@users.noreply.github.com>
2022-03-25 12:40:12 -0400

    Update mySQLconnector.py
    Final Addition

--- mySQLconnector.py
+++ mySQLconnector.py
@@ -1,22 +1,37 @@
 
+class FakeConnect:
+	def __init__(self, host, user, password):
+		self.host = host
+		self.user = user
+		self.password = password
+
+	def cursor(self):
+		return Cursor()
+
+class Cursor:
+	def __init__(self):
+		pass
+
+	def execute(self, operation, params=None, mulit=False):
+		return None		
+
 class MySqlConnection:
 	def __init__(self, user, password, host="localhost"):
 
 		# Connect to a MySQL database		
-		self.db = mysql.connector.connect(host=host, user=user, password=password)
+		self.db = FakeConnect(host, user, password)
+		# self.db = mysql.connector.connect(host=host, user=user, password=password)
 
 		# Initialize a cursor object that executes SQL operations
 		self.cursor = self.db.cursor()
 
 	def execute(self, commandString):
-		self.cursor.execute(commandString)
 
-		return None
+		# Perform neccesary checks
+		if commandString == None or commandString == "":
+			return False
 
-	def print_tables(self):
-		self.execute("SHOW TABLES")
-
-		for table in self.cursor:
-			print(table)
-
+		else:
+			self.cursor.execute(commandString)
+			return True
 	
