e20264de1c0ca6ae2247f5a3e93db3f48219d416
Ryan Kim <43964171+Ryan-JW-Kim@users.noreply.github.com>
2022-03-25 12:39:49 -0400

    Update main.py
    Final Addition

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
+from tkinter import Y
+from userInputManager import *
+from mySQLconnector import * 
+
+USER = "Test"
+PASSWORD = "TEST"
 
 def main():
 	
+	ui = UserInput()
+	SQL_connection = MySqlConnection(user=USER, password=PASSWORD)
 
+	while ui.breakpoint == False:
+
+		myDict = ui.receive_command()
+
+		if ui.breakpoint == False:
+			myQuery = ui.parse_to_SQL(myDict)
+
+			print(f"\nSQL Query --> {myQuery} <--")
+
+			code = SQL_connection.execute(myQuery)
+
+			if code == False:
+				print("		Error In Execution")
+
+			else:
+				print("		Success\n")
 
 if __name__ == "__main__":
+
+	print(f"----Starting User-DB interaction----")
 	main()
+	print(f"----Ending User-DB interaction------")
