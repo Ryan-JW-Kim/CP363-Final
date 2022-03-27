8bdcce043effe0d3d4ed218d523aed720596b591
Ryan Kim <43964171+Ryan-JW-Kim@users.noreply.github.com>
2022-03-27 17:16:17 -0400

    Update mySQLconnector.py

--- mySQLconnector.py
+++ mySQLconnector.py
@@ -19,8 +19,7 @@ class MySqlConnection:
 	def __init__(self, user, password, host="localhost"):
 
 		# Connect to a MySQL database		
-		self.db = FakeConnect(host, user, password)
-		# self.db = mysql.connector.connect(host=host, user=user, password=password)
+		self.db = FakeConnect(host, user, password) # self.db = mysql.connector.connect(host=host, user=user, password=password)
 
 		# Initialize a cursor object that executes SQL operations
 		self.cursor = self.db.cursor()
@@ -34,4 +33,5 @@ class MySqlConnection:
 		else:
 			self.cursor.execute(commandString)
 			return True
-	
+
+
